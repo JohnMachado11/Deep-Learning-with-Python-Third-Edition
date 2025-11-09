661f674c327d71fc31498dcadd39cd749209b4c4
John Machado <iamjohnmachado@gmail.com>
2025-11-09 11:20:28 -0600

    What is Deep Learning

---
+++ 01_What_is_Deep_Learning/resources.py
@@ -0,0 +1,11 @@
+# What Are Neural Networks Even Doing? (Manifold Hypothesis)
+# https://www.youtube.com/watch?v=pdNYw6qwuNc
+
+# Visualizing Neural Network Training and Predictions: A Universal Function Approximator
+# https://www.youtube.com/watch?v=xg4bIeJTVF0&list=FLyoWHihDKlNs3uq7rBuYOyw
+
+# My Understanding of the Manifold Hypothesis ft Geoffrey Hinton | Deep Learning
+# https://www.youtube.com/watch?v=BePQBWPnYuE
+
+# How to Flatten Your Manifold
+# https://www.youtube.com/watch?v=BHUmkr9apSM
